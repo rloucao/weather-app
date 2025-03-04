6ff1f2a928ccd55720c93edb167c1317317300cf
MoedaDe5Euros <rm.loucao@campus.fct.unl.pt>
2025-03-04 21:28:19 +0000

    Minor adjustments

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { GeistSans } from "geist/font/sans";
 import { type AppType } from "next/app";
-
+import Head from "next/head";
+import icon from "../../public/weather-icon2.png"
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
@@ -8,6 +9,10 @@ import "~/styles/globals.css";
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <div className={GeistSans.className}>
+      <Head>
+        <title>Weather</title>
+        <link rel="icon" href={icon.src} />
+      </Head>
       <Component {...pageProps} />
     </div>
   );
